924f2ac4588d6f5b0e77476a03e0571fd93f5d81
NikaQanashvili <qanashvilinika7@gmail.com>
2025-04-21 18:17:33 +0400

    feat: add new function to change order status

--- controllers/orderController.js
+++ controllers/orderController.js
@@ -65,3 +65,34 @@ export const deleteOrder = async (req, res) => {
     res.status(500).json({ message: "Error deleting order" });
   }
 };
+
+export const updateOrderStatus = async (req, res) => {
+  const { id } = req.params;
+  const { status } = req.body;
+  const statusRecord = await prisma.order_status.findUnique({
+    where: {
+      name: status,
+    },
+  });
+
+  if (!statusRecord) {
+    return res.status(404).json({ error: "Status not found" });
+  }
+
+  try {
+    const order = await prisma.orders.update({
+      where: { id: parseInt(id) },
+      data: {
+        order_status_id: statusRecord.id,
+      },
+      include: {
+        order_status: true,
+      },
+    });
+    res
+      .status(200)
+      .json({ message: "Order updated successfully", data: order });
+  } catch (error) {
+    res.status(500).json({ error: error.message });
+  }
+};
--- routes/orderRoutes.js
+++ routes/orderRoutes.js
@@ -8,9 +8,16 @@ import {
   createOrder,
   updateOrder,
   deleteOrder,
+  updateOrderStatus,
 } from "../controllers/orderController.js";
+import { auth, isSales } from "../middlewares/auth.js";
 
-router.route("/").get(getOrders).post(createOrder);
-router.route("/:id").get(getOrderById).put(updateOrder).delete(deleteOrder);
+router.route("/").get(getOrders).post(auth, isSales, createOrder);
+router
+  .route("/:id")
+  .get(getOrderById)
+  .put(auth, isSales, updateOrder)
+  .delete(deleteOrder);
+router.route("/:id/status").put(auth, isSales, updateOrderStatus);
 
 export default router;
